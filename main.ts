465fdfdb6e9cc332221763808a66b8b26f6b11d0
zhourg2010 <132064123+zhourg2010@users.noreply.github.com>
2026-06-09 16:03:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
 import { serveFile } from "jsr:@std/http/file-server";
 
 Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+  const url = new URL(req.url);
+
+  // 访问 /s/test 就返回节点内容(先用假数据)
+  if (url.pathname === "/s/test") {
+    const fakeNodes = "vmess://这里以后是真节点1\nvmess://这里以后是真节点2";
+    return new Response(fakeNodes, {
+      headers: { "content-type": "text/plain; charset=utf-8" },
+    });
+  }
+
+  // 其他情况,还是返回原来那个网页
+  return serveFile(req, "./index.html");
 });
